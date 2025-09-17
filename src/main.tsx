e9b3328a1b8a93484b668032673f0c7e779965c8
maximiliano <majf2704@gmail.com>
2025-09-17 00:51:36 -0300

    add app version

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { ProjectProvider } from './context/projectContext.tsx'
 
+
+const APP_VERSION = "1.0.3";
+
+if (localStorage.getItem("app_version") !== APP_VERSION) {
+  localStorage.clear();
+  localStorage.setItem("app_version", APP_VERSION);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ProjectProvider>
