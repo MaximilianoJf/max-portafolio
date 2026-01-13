d63b72430af7831671e04fb26fc4519deea0e7d1
MaximilianoJM <majf2704@gmail.com>
2026-01-12 22:45:36 -0300

    arreglando footer

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,26 +6,26 @@ import WaveSvg from "../components/ui/WaveSvg"
 export default function Layout() {
   return (
     <>
-      <div className="relative min-h-screen flex gap-y-5 flex-col">
+      <div className="relative min-h-screen flex flex-col">
         <Header />
-        <main className="relative z-10 items-center justify-center">
 
-          <div className="flex flex-col mt-12 gap-y-10 max-w-7xl mx-auto sm:px-6 sm:py-6 ">
+
+        <main className="flex-grow relative z-10 p-4">
+          <div className="flex flex-col mt-12 gap-y-10 max-w-7xl mx-auto sm:px-6 sm:py-6">
             <Outlet />
           </div>
-
-          <Footer />
-
           <ProjectImageViewer />
-
         </main>
+
+        <footer className="relative z-20 w-full">
+          <Footer />
+        </footer>
+
 
         <div className="fixed bottom-0 left-0 w-full z-0 pointer-events-none">
           <WaveSvg />
         </div>
-
       </div>
-
     </>
   )
 }
